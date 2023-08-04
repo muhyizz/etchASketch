a45d3cd1099417637278e8c662380ed57d6a3b19
muhyizz <muhyizz.mohammed@gmail.com>
2023-08-05 02:43:42 +0800

    complete everything except css

---
+++ script.js
@@ -0,0 +1,52 @@
+let color = 'black'
+let click = false;
+
+function populateBoard(size){
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    board.style.gridTemplateRows = `repeat(${size},1fr)`;
+    let amount = size * size;
+    for (let i = 0; i < amount; i++ ){
+        let square = document.createElement("div");
+        square.addEventListener('mouseover', colorSquare)
+        square.style.backgroundColor = "white";
+        board.insertAdjacentElement("beforeend", square);
+    }
+}
+
+populateBoard(16);
+
+function changeSize(input){
+    if(input >=2 && input <= 100){
+        populateBoard(input);
+    } else {
+        console.log("too many squares");
+    }
+    
+}
+
+function colorSquare(){
+    if(click){
+        if(color === "random"){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = color;
+        }
+    }
+}
+
+function changeColor(choice){
+    color = choice;
+}
+
+function resetBoard(){
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+
+document.querySelector('.board').addEventListener("click", ()=>{
+    click = !click;
+})
